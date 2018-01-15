7a74f866602c7dfcf50244f0af98f2bc49a428fe
Riccardo <Riccardo@192.168.1.31>
2018-01-15 14:03:23 +0100

    modificata icona aggiunta pagina informazioni

---
+++ client-side/js/info.js
@@ -0,0 +1,12 @@
+var info = {
+    
+    // Bottone home
+    init_home: function() {
+        $('#home').on('click', function() {
+            window.location.href = '/home';
+        });
+    }
+    
+};
+
+$(document).ready(info.init_home());
