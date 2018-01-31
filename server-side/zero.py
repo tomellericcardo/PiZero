6403098b76195dfaf4870d4ff2cdaa370fad01d5
Riccardo <Riccardo@192.168.1.31>
2018-01-31 11:56:29 +0100

    aggiunto download album #6

--- server-side/zero.py
+++ server-side/zero.py
@@ -118,7 +118,7 @@ class Zero:
     # Download dell'album
     def download(self):
         self.lock.acquire()
-        comando = 'zip ' + self.percorso + 'album/archivio.zip ' + self.percorso + 'album/*'
+        comando = 'zip ' + self.percorso + 'album/album.zip ' + self.percorso + 'album/*'
         call(comando, shell = True)
         self.lock.release()
     
